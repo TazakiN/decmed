0996292a0212b4eefeb045712c6bb6ea82c0660e
TazakiN <tazkia.nizami@gmail.com>
2026-02-10 21:40:24 +0700

    chore: remove sc

--- sc/ignition/modules/Hospital.js
+++
@@ -1,9 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
-const HospitalModule = buildModule("HospitalModule", (m) => {
-  const hospital = m.contract("Hospital");
-
-  return { hospital };
-});
-
-module.exports = HospitalModule;
